4311c742e616174493fc30fe0c55ff50d0ed3ddb
Kawan Paiva <165952625+kawanzx@users.noreply.github.com>
2024-09-06 11:06:23 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,14 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
+
+let botao = document.querySelector('.meu-botao');
+let paragrafos = document.querySelectorAll('p');
